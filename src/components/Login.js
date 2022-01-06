52d472cf9cabc36c1e659f9277df013c5b59416b
Matthew McKney <mrmckney@gmail.com>
2022-01-05 20:00:27 -0500

    fixed login and signup bugs

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,67 +11,71 @@ function Login({isLogin, setIsLogin, handleClose, show}) {
     const [userCreds, setUserCreds] = useState(null)
     
     const handleSignIn = () => {
-        if(userCreds === null){
+        if(!userCreds){
             alert('Missing username or password')
             return 
         }
         const {username, password} = userCreds
-        if(username === "" || password === "" ){
+        if(!username || !password){
             alert('Missing username or password')
             return
         }
-        const hashedPassword = bcrypt.hashSync(password, mySalt)
-        fetch(`${process.env.REACT_APP_API_ENDPOINT}/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({username, password: hashedPassword})
-        })
-        .then(response => response.json())
-        .then(data => {
-            if(data.status !== 200){
-                alert(data.message)
-                return
-            }
-            if(data.message !== 'Login Attempt Failed'){
-                handleClose(false)
-            }
-            setDisplayName(data.user.username)
-            setUser(data.token)
-            localStorage.setItem('user', data.token)
-            localStorage.setItem('displayname', data.user.username)
-        })
-        .catch(err => alert(err))
+        if(username && password){
+            const hashedPassword = bcrypt.hashSync(password, mySalt)
+            fetch(`${process.env.REACT_APP_API_ENDPOINT}/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username, password: hashedPassword})
+            })
+            .then(response => response.json())
+            .then(data => {
+                if(data.status !== 200){
+                    alert(data.message)
+                    return
+                }
+                if(data.message !== 'Login Attempt Failed'){
+                    handleClose(false)
+                }
+                setDisplayName(data.user.username)
+                setUser(data.token)
+                localStorage.setItem('user', data.token)
+                localStorage.setItem('displayname', data.user.username)
+            })
+            .catch(err => alert(err))
+        }
     }
 
     const handleSignUp = () => {
-        if(userCreds === null){
+        if(!userCreds){
             alert('Missing username or password')
             return 
         }
         const {username, password} = userCreds
-        if(username === "" || password === "" ){
+        if(!username || !password){
             alert('Missing username or password')
             return
         }
-        const hashedPassword = bcrypt.hashSync(password, mySalt)
-        fetch(`${process.env.REACT_APP_API_ENDPOINT}/signup`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({username, password: hashedPassword})
-        })
-        .then(response => response.json())
-        .then(data => {
-            setDisplayName(data.user.username)
-            setUser(data.token)
-            localStorage.setItem('user', data.token)
-            localStorage.setItem('displayname', data.user.username)
-        })
-        .then(() => handleClose(false))
-        .catch(err => alert(err))
+        if(username && password){
+            const hashedPassword = bcrypt.hashSync(password, mySalt)
+            fetch(`${process.env.REACT_APP_API_ENDPOINT}/signup`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username, password: hashedPassword})
+            })
+            .then(response => response.json())
+            .then(data => {
+                setDisplayName(data.user.username)
+                setUser(data.token)
+                localStorage.setItem('user', data.token)
+                localStorage.setItem('displayname', data.user.username)
+            })
+            .then(() => handleClose(false))
+            .catch(err => alert(err))
+    }
     }
 
     const handleForm = e => {
